a3ee192618e7257e1191b9d62896d1138858a375
Janindu-Pathirana <pathiranaJanindu@gmail.com>
2022-09-12 08:00:27 +0530

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Html5Qrcode } from 'html5-qrcode'
+import styled from "styled-components";
 
 function App() {
+  const [cameraId, setCameraId] = useState('');
+  let html5QrCode : Html5Qrcode;
+
+  const getPermition = () => {
+    Html5Qrcode.getCameras()
+      .then((devices) => {
+        if (devices && devices.length) {
+          setCameraId(devices[0].id)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const start = () => {
+    html5QrCode = new Html5Qrcode('reader')
+    html5QrCode
+      .start(
+        cameraId,
+        {
+          fps: 10, 
+          qrbox: { width: 250, height: 250 },
+        },
+        (decodedText, decodedResult) => {
+          console.log('decodedText: ', decodedText)
+          console.log('decodedResult: ', decodedResult)
+        },
+        (errorMessage) => {
+         console.log(errorMessage);
+        },
+      )
+      .catch((err) => {
+        console.log("start fail", err);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CamRange id="reader"></CamRange>
+      <Button onClick={(e) => getPermition()}>permitions</Button>
+      <Button onClick={(e) => start()}>start</Button>
+      <Button
+        onClick={(e) => {
+          html5QrCode.stop()
+        }}
+      >
+        stop
+      </Button>
     </div>
   );
 }
+
+
+const CamRange = styled.div`
+  width: 500px;
+  height: 500px;
+`
+
+const Button = styled.div`
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 5px 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px;
+`
 
 export default App;
